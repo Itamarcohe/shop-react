a3b17fe0d4cbddac1a0e4a496de7d14362b6bb84
Itamar Cohen <Itamar330@gmail.com>
2022-04-15 00:44:48 +0300

    react on heroku

--- src/components/Urls.js
+++ src/components/Urls.js
@@ -1,7 +1,13 @@
+// const URLS = {
+//   base_url: "http://127.0.0.1:8000/api/",
+//   all_products: "http://127.0.0.1:8000/api/products",
+//   all_categories: "http://127.0.0.1:8000/api/category_links",
+// };
+
 const URLS = {
-  base_url: "http://127.0.0.1:8000/api/",
-  all_products: "http://127.0.0.1:8000/api/products",
-  all_categories: "http://127.0.0.1:8000/api/category_links",
+  base_url: "https://itamar-shop.herokuapp.com/",
+  all_products: "https://itamar-shop.herokuapp.com/api/products",
+  all_categories: "https://itamar-shop.herokuapp.com/api/category_links",
 };
 
 export default URLS;
