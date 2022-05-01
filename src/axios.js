680d4bf9a45970627bbff7dc49ea7e01fabf7451
Itamar Cohen <Itamar330@gmail.com>
2022-05-01 03:15:26 +0300

    check

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://127.0.0.1:8000/api/";
-const baseURL = "https://itamarshop.herokuapp.com/api/";
+const baseURL = "https://itamar-shop.herokuapp.com/api/";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
